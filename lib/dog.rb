292eb88da91c7984ce6783b8dfa56db2edaa44c0
Netaly Ramirez <21netaly@gmail.com>
2020-03-13 23:44:43 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def initialize (name)
+    @name = name
+  end
+
+  def names
+   @name
+  end
+  # def initialize(breed)
+  #   @breed = breeds
+  # end
+  def breeds
+    @breed
+  end
+end
